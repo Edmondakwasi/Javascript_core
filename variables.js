63a2fe329d929dc1e2de4f659ec466badfdee836
Edmond Akwasi <107095324+Edmondakwasi@users.noreply.github.com>
2024-07-13 01:18:31 +0000

    added lessions

---
+++ variables.js
@@ -0,0 +1,61 @@
+// var title = "Javascript"
+
+// description = "A javascript"
+
+// console.log(title);
+
+// console.log(description);
+
+// let a = 10;
+
+// function gitname(){
+//     console.log("John Doe");
+// }
+
+// gitname();
+// console.log(a);
+
+
+let str = "   Javascript   "
+
+console.log(str);
+
+let len = str.length;
+
+console.log(len);
+
+console.log(str[0]); 
+
+console.log(str[len-1]);
+
+console.log(str.charAt(0));
+
+console.log("After applying trimmed")
+
+let trimedstr = str.trim();
+let trimmedlen = trimedstr.length;
+
+console.log(trimedstr);
+
+console.log(trimmedlen);
+
+let cut = str.slice(1, 2);
+
+console.log(cut);
+
+
+
+let name = 'Edmond';
+
+let age = 30;
+
+let aboutme = `Hey everyone my name is ${name} and my age is ${age}`;
+
+console.log(aboutme);
+
+let str1 = "this is my locaation";
+let str2 = "and this is the additon";
+
+console.log(str1+str2);
+
+
